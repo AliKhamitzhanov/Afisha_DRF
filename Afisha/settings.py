b49537f8437f514b82bbee6a34da9fea84c0c6cc
AliKhamitzhanov <doktordrakonchik@gmail.com>
2022-11-11 02:58:59 +0600

    add postgresgl

--- Afisha/settings.py
+++ Afisha/settings.py
@@ -85,8 +85,12 @@ WSGI_APPLICATION = 'Afisha.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': 'book_user',
+        'PASSWORD': '123456',
+        'PORT': 5432,
+        'HOST': 'localhost'
     }
 }
 
